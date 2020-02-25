80809fc045c52f646a1350d0a86b04859b0d98c0
= <Miro.Hud@gmail.com>
2020-02-25 13:45:05 +0100

    singlylinkedlist changes

---
+++ ds/singly_linked_list.py
@@ -0,0 +1,139 @@
+from typing import Any, Optional
+import sys
+
+
+class Node:
+
+    def __init__(self, data: Any, next: 'Node' = None):
+        self.data: Any = data
+        self.next: 'Node' = next
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.size: int = 0
+        self.head: Optional['Node'] = None
+        self.tail: Optional['Node'] = None
+
+    def add_last(self, value):
+        if self.is_empty:
+            self.head = self.tail = Node(data=value)
+        else:
+            self.tail.next = Node(data=value)
+            self.tail = self.tail.next
+        self.size += 1
+
+    def add_first(self, value):
+        if self.is_empty:
+            self.head = self.tail = Node(data=value)
+        else:
+            self.head = Node(data=value, next=self.head)
+        self.size += 1
+
+    def add(self, value: Any, index: int):
+        """
+        Add value at the specified index
+        :param value: value to add
+        :param index: specifies at what index to add the value
+        :return: None
+        """
+        if index > self.size or index < 0:
+            raise IndexError()
+        if index == 0:
+            self.add_first(value)
+        elif index == self.size:
+            self.add_last(value)
+            # adding in-between
+        else:
+            position = 1
+            trav = self.head
+            while trav:
+                if position == index:
+                    next_trav = Node(data=value, next=trav.next)
+                    trav.next = next_trav
+                    break
+                trav = trav.next
+                position += 1
+        self.size += 1
+
+    def remove(self, index: int) -> Any:
+        if index < 0 or index > self.size:
+            raise IndexError()
+        if index == 0:
+            self.remove_first()
+        elif index == self.size:
+            self.remove_last()
+        else:
+            position: int = 1
+            trav: 'Node' = self.head
+            while trav:
+                if position == index:
+                    node = trav.next.next
+                    removed = trav.next.data
+                    trav.next.next = None
+                    trav.next = node
+                    return removed
+                trav = trav.next
+                position += 1
+        self.size -= 1
+
+    def peek(self, index):
+        pass
+
+    def remove_last(self):
+        if self.is_empty:
+            raise RuntimeError('Empty list')
+        trav = self.head
+        while trav:
+            if trav.next == self.tail:
+                trav.next = None
+                self.tail = trav
+            trav = trav.next
+        self.size -= 1
+
+    def remove_first(self):
+        if self.is_empty:
+            raise RuntimeError('Empty list')
+        #  get the next node
+        node = self.head.next
+        #  remove all the first node references
+        self.head.next = None
+        #  set new head
+        self.head = node
+        self.size -= 1
+
+    @property
+    def is_empty(self):
+        return not bool(self.size)
+
+    def peek_first(self):
+        if self.is_empty:
+            return None
+        return self.head.data
+
+    def peek_last(self):
+        if self.is_empty:
+            return None
+        return self.tail.data
+
+    def __str__(self):
+        trav = self.head
+        output = '['
+        while trav:
+            if trav.next:
+                output += f'{str(trav.data)}, '
+            else:
+                output += f'{str(trav.data)}'
+            trav = trav.next
+        output += ']'
+        return output
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        trav = self.head
+        while trav:
+            yield trav.data
+            trav = trav.next
---
+++ tests/ds/test_singly_linked_list.py
@@ -0,0 +1,109 @@
+import unittest
+
+from ds.singly_linked_list import SinglyLinkedList
+
+
+class TestSinglyLinkedList(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.list = SinglyLinkedList()
+        self.list.add_last(5)
+        self.list.add_last(10)
+        self.list.add_last(7)
+        self.list.add_last(8)
+        self.list.add_last(1)
+
+    def test_length_of_list(self):
+        self.assertEqual(len(self.list), 5)
+
+    def test_str_of_list(self):
+        self.assertEqual(str(self.list), '[5, 10, 7, 8, 1]')
+
+    def test_iterator_list(self):
+        self.assertEqual([x for x in self.list], [5, 10, 7, 8, 1])
+
+    def test_add_first_empty(self):
+        _list = SinglyLinkedList()
+        _list.add_first(10)
+        self.assertEqual([x for x in _list], [10])
+
+    def test_add_last_empty(self):
+        _list = SinglyLinkedList()
+        _list.add_last(10)
+        self.assertEqual([x for x in _list], [10])
+
+    def test_add_last_values(self):
+        self.list.add_last(15)
+        self.assertEqual([x for x in self.list], [5, 10, 7, 8, 1, 15])
+
+    def test_add_first_values(self):
+        self.list.add_first(15)
+        self.assertEqual([x for x in self.list], [15, 5, 10, 7, 8, 1])
+
+    def test_remove_last(self):
+        self.list.remove_last()
+        self.assertEqual([x for x in self.list], [5, 10, 7, 8])
+
+    def test_remove_last_from_empty(self):
+        empty_list = SinglyLinkedList()
+        with self.assertRaises(RuntimeError):
+            empty_list.remove_last()
+
+    def test_remove_first(self):
+        self.list.remove_first()
+        self.assertEqual([x for x in self.list], [10, 7, 8, 1])
+
+    def test_remove_first_from_empty(self):
+        empty_list = SinglyLinkedList()
+        with self.assertRaises(RuntimeError):
+            empty_list.remove_first()
+
+    def test_is_not_empty(self):
+        self.assertFalse(self.list.is_empty)
+
+    def test_is_empty(self):
+        _list = SinglyLinkedList()
+        self.assertTrue(_list.is_empty)
+
+    def test_peek_first(self):
+        self.assertEqual(self.list.peek_first(), 5)
+
+    def test_peek_first_empty(self):
+        _list = SinglyLinkedList()
+        self.assertIsNone(_list.peek_first())
+
+    def test_peek_last(self):
+        self.assertEqual(self.list.peek_last(), 1)
+
+    def test_peek_last_empty(self):
+        _list = SinglyLinkedList()
+        self.assertIsNone(_list.peek_last())
+
+    def test_add_at_index(self):
+        self.list.add(20, 2)
+        self.assertEqual([x for x in self.list], [5, 10, 20, 7, 8, 1])
+
+    def test_add_at_index_too_low(self):
+        with self.assertRaises(IndexError):
+            self.list.add(10, -1)
+
+    def test_add_at_index_too_high(self):
+        with self.assertRaises(IndexError):
+            self.list.add(10, 10)
+
+    def test_add_at_index_first(self):
+        _list = SinglyLinkedList()
+        _list.add(1, 0)
+        self.assertEqual([x for x in _list], [1])
+
+    def test_add_at_index_last(self):
+        _list = SinglyLinkedList()
+        _list.add_last(1)
+        _list.add_last(10)
+        _list.add(5, 2)
+        self.assertEqual([x for x in _list], [1, 10, 5])
+
+    def test_remove_at_index(self):
+        data = self.list.remove(2)
+        self.assertEqual(data, 7)
+        self.assertEqual([x for x in self.list], [5, 10, 8, 1])
