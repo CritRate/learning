764a04ba4f66e8c0b6a42361c44e015534d2fde5
= <Miro.Hud@gmail.com>
2020-02-27 18:22:15 +0100

    finished doubly linked list - comments,docstring and tests

--- ds/doubly_linked_list.py
+++ ds/doubly_linked_list.py
@@ -32,9 +32,6 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
-    @classmethod
-    def from_list(cls):
-
     def add_last(self, data: object):
         """
         Add data to the end of the list
@@ -74,7 +71,7 @@ class DoublyLinkedList:
         :return:
         """
         if index < 0 or index > self.size:
-            return IndexError()
+            raise IndexError()
         elif index == 0:
             self.add_first(data=data)
         elif index == self.size:
@@ -107,6 +104,141 @@ class DoublyLinkedList:
                     position -= 1
         self.size += 1
 
+    def peek_first(self) -> object:
+        """
+        Returns the first data in the list without removing it. If the list is empty, returns None.
+
+        :return: Data at the start of the list
+        :rtype: object
+        """
+        if self.is_empty:
+            return None
+        return self.head.data
+
+    def peek_last(self) -> object:
+        """
+        Returns the last data in the list without removing it. If the list is empty, returns None.
+
+        :return: Data at the end of the list
+        ":rtype" object
+        """
+        if self.is_empty:
+            return None
+        return self.tail.data
+
+    def peek(self, index) -> object:
+        """
+        Returns the data at the specified index without removing it.
+
+        :param index: Position of the item in the list
+        :return: Data at the specified index
+        :rtype: object
+        """
+        if index < 0 or index >= self.size:
+            raise IndexError()
+        elif index == 0:
+            return self.peek_first()
+        elif index == self.size - 1:
+            return self.peek_last()
+        else:
+            if index < self.size / 2:
+                for i, data in enumerate(self):
+                    if i == index:
+                        return data
+            else:
+                position = self.size - 1
+                for i, data in enumerate(reversed(self)):
+                    position -= i
+                    if position == index:
+                        return data
+
+    def remove_last(self) -> object:
+        """
+        Remove data at the end of the list and return it.
+
+        :return: Data at the end of the list
+        :rtype: object
+        """
+        if self.is_empty:
+            raise RuntimeError('List is empty')
+
+        removed = self.tail.data
+        if self.tail.prev:
+            node = self.tail.prev
+            self.tail.prev.next = None
+            self.tail.prev = None
+            self.tail = node
+        else:
+            self.head = None
+            self.tail = None
+        self.size -= 1
+        return removed
+
+    def remove_first(self) -> object:
+        """
+        Remove data at the start of the list and return it.
+
+        :return: Data at the start of the list
+        :rtype: object
+        """
+        if self.is_empty:
+            raise RuntimeError('List is empty')
+        removed = self.head.data
+        if self.head.next:
+            node = self.head.next
+            self.head.next.prev = None
+            self.head.next = None
+            self.head = node
+        else:
+            self.head = None
+            self.tail = None
+        self.size -= 1
+        return removed
+
+    def remove(self, index: int) -> object:
+        """
+        Remove data at the specified index and return it.
+
+        :param index: Position of the data in the list
+        :return: Data at the specified index
+        :rtype: object
+        """
+        if index < 0 or index >= self.size:
+            raise IndexError()
+        elif index == 0:
+            return self.remove_first()
+        elif index == self.size - 1:
+            return self.remove_last()
+        else:
+            if index < self.size / 2:
+                trav = self.head.next
+                position = 1
+                while trav:
+                    if index == position:
+                        removed = trav.data
+                        trav.prev.next = trav.next
+                        trav.next.prev = trav.prev
+                        trav.next = None
+                        trav.prev = None
+                        self.size -= 1
+                        return removed
+                    trav = trav.next
+                    position += 1
+            else:
+                trav = self.tail
+                position = self.size - 1
+                while trav:
+                    if index == position:
+                        removed = trav.data
+                        trav.prev.next = trav.next
+                        trav.next.prev = trav.prev
+                        trav.next = None
+                        trav.prev = None
+                        self.size -= 1
+                        return removed
+                    trav = trav.prev
+                    position -= 1
+
     def clear(self):
         """
         Remove every element from the list
@@ -116,6 +248,7 @@ class DoublyLinkedList:
         trav = self.head
         while trav:
             node = trav.next
+            # remove every Node references to prev and next Node
             trav.prev = None
             trav.next = None
             trav = node
--- tests/ds/test_doubly_linked_list.py
+++ tests/ds/test_doubly_linked_list.py
@@ -54,6 +54,11 @@ class TestDoubleLinkedList(unittest.TestCase):
         self.assertEqual([x for x in self.list], [0, 1, 2, 3, 4, 5])
         self.assertEqual([x for x in reversed(self.list)], [5, 4, 3, 2, 1, 0])
 
+    def test_add_first_empty(self):
+        _list = DoublyLinkedList()
+        _list.add_first(1)
+        self.assertEqual([x for x in _list], [1])
+
     def test_add_at_index_lower_half(self):
         self.list.add(6, 2)
         self.assertEqual([x for x in self.list], [1, 2, 6, 3, 4, 5])
@@ -64,7 +69,140 @@ class TestDoubleLinkedList(unittest.TestCase):
         self.assertEqual([x for x in self.list], [1, 2, 3, 4, 6, 5])
         self.assertEqual([x for x in reversed(self.list)], [5, 6, 4, 3, 2, 1])
 
+    def test_add_at_index_too_low(self):
+        with self.assertRaises(IndexError):
+            self.list.add(6, -1)
+
+    def test_add_at_index_too_high(self):
+        with self.assertRaises(IndexError):
+            self.list.add(6, len(self.list) + 1)
+
+    def test_add_at_index_zero(self):
+        self.list.add(0, 0)
+        self.assertEqual([x for x in self.list], [0, 1, 2, 3, 4, 5])
+
+    def test_add_at_index_max(self):
+        self.list.add(6, len(self.list))
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5, 6])
+
     def test_clear(self):
         self.list.clear()
         self.assertEqual([x for x in self.list], [])
         self.assertEqual(len(self.list), 0)
+
+    def test_peek_last(self):
+        self.assertEqual(self.list.peek_last(), 5)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_first(self):
+        self.assertEqual(self.list.peek_first(), 1)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_last_empty(self):
+        _list = DoublyLinkedList()
+        self.assertIsNone(_list.peek_last())
+
+    def test_peek_first_empty(self):
+        _list = DoublyLinkedList()
+        self.assertIsNone(_list.peek_first())
+
+    def test_peek_lower(self):
+        self.assertEqual(self.list.peek(1), 2)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_upper(self):
+        self.assertEqual(self.list.peek(3), 4)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_zero(self):
+        self.assertEqual(self.list.peek(0), 1)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_max(self):
+        self.assertEqual(self.list.peek(len(self.list) - 1), 5)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4, 5])
+
+    def test_peek_empty_list(self):
+        with self.assertRaises(IndexError):
+            _list = DoublyLinkedList()
+            _list.peek(0)
+
+    def test_peek_too_low(self):
+        with self.assertRaises(IndexError):
+            self.list.peek(-1)
+
+    def test_peek_too_high(self):
+        with self.assertRaises(IndexError):
+            self.list.peek(len(self.list))
+
+    def test_remove_last_one_item(self):
+        _list = DoublyLinkedList()
+        _list.add_first(1)
+        self.assertEqual([x for x in _list], [1])
+        data = _list.remove_last()
+        self.assertEqual(data, 1)
+        self.assertEqual([x for x in _list], [])
+        self.assertEqual([x for x in reversed(_list)], [])
+
+    def test_remove_last_empty(self):
+        with self.assertRaises(RuntimeError):
+            _list = DoublyLinkedList()
+            _list.remove_last()
+
+    def test_remove_last(self):
+        data = self.list.remove_last()
+        self.assertEqual(data, 5)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4])
+        self.assertEqual([x for x in reversed(self.list)], [4, 3, 2, 1])
+
+    def test_remove_first_one_item(self):
+        _list = DoublyLinkedList()
+        _list.add_first(1)
+        self.assertEqual([x for x in _list], [1])
+        data = _list.remove_first()
+        self.assertEqual(data, 1)
+        self.assertEqual([x for x in _list], [])
+        self.assertEqual([x for x in reversed(_list)], [])
+
+    def test_remove_first_empty(self):
+        with self.assertRaises(RuntimeError):
+            _list = DoublyLinkedList()
+            _list.remove_first()
+
+    def test_remove_first(self):
+        data = self.list.remove_first()
+        self.assertEqual(data, 1)
+        self.assertEqual([x for x in self.list], [2, 3, 4, 5])
+        self.assertEqual([x for x in reversed(self.list)], [5, 4, 3, 2])
+
+    def test_remove_lower(self):
+        data = self.list.remove(2)
+        self.assertEqual(data, 3)
+        self.assertEqual([x for x in self.list], [1, 2, 4, 5])
+        self.assertEqual([x for x in reversed(self.list)], [5, 4, 2, 1])
+
+    def test_remove_upper(self):
+        data = self.list.remove(3)
+        self.assertEqual(data, 4)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 5])
+        self.assertEqual([x for x in reversed(self.list)], [5, 3, 2, 1])
+
+    def test_remove_index_too_low(self):
+        with self.assertRaises(IndexError):
+            self.list.remove(-1)
+
+    def test_remove_index_too_high(self):
+        with self.assertRaises(IndexError):
+            self.list.remove(len(self.list))
+
+    def test_remove_at_index_first(self):
+        data = self.list.remove(0)
+        self.assertEqual(data, 1)
+        self.assertEqual([x for x in self.list], [2, 3, 4, 5])
+        self.assertEqual([x for x in reversed(self.list)], [5, 4, 3, 2])
+
+    def test_remove_at_index_end(self):
+        data = self.list.remove(len(self.list) - 1)
+        self.assertEqual(data, 5)
+        self.assertEqual([x for x in self.list], [1, 2, 3, 4])
+        self.assertEqual([x for x in reversed(self.list)], [4, 3, 2, 1])
